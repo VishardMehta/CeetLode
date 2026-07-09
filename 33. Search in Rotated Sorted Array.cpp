4625692573f133cef288232856ee1fd5ee513acb
Vishard Mehta <132185989+VishardMehta@users.noreply.github.com>
2026-07-09 16:34:18 +0530

    Update 33. Search in Rotated Sorted Array.cpp

--- 33. Search in Rotated Sorted Array.cpp
+++ 33. Search in Rotated Sorted Array.cpp
@@ -11,12 +11,14 @@ public:
         if(nums[mid]==target){
             return mid;
         }
+            //left half is sorted , we go to right half to find the element
 
         if(nums[low]<=nums[mid]){
             if(nums[low]<=target && target<nums[mid]) high=mid-1;
 
             else low=mid+1;
         }
+            // right half is sorted we got to left side to find the element and mid finds the element always 
         else{
             if( target<=nums[high] && nums[mid]<target) low=mid+1;
 
